16af6d7285408cd6e61ae10941a81f8123ea9074
hugo-chan <hugoyhc14@gmail.com>
2022-01-08 01:24:36 -0600

    condensed to 7 lines

--- ez.py
+++ ez.py
@@ -1,20 +1,7 @@
 from bs4 import BeautifulSoup
 import pyautogui
 import time
-
 with open("test.html", encoding='utf-8') as f:
-    soup = BeautifulSoup(f, "html.parser")
-
-words = soup.find(id="words")
-words_ls = []
-for word in words:
-    word_chars = []
-    for letter in word:
-        word_chars.append(letter.text)
-    words_ls.append(word_chars)
-
-word_str =  ' '.join([''.join(word) for word in words_ls])
-print("word_str", word_str)
-
-time.sleep(1)
+    word_str = ' '.join([''.join([letter.text for letter in word]) for word in BeautifulSoup(f, "html.parser").find(id="words")])
+time.sleep(1.2)
 pyautogui.write(word_str, interval=0.01)
